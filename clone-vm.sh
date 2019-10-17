aeb62aa6eddf203fdb4864b5b980f3922656746f
Adad <adadjiramosoriohernandez9@gmail.com>
2019-10-17 13:54:49 -0500

    Update clone-vm.sh

--- clone-vm.sh
+++ clone-vm.sh
@@ -44,7 +44,7 @@ clear
 # Test if we receive the VM Name
 #
 
-if [[ test -z "${vm_nm}" ]] || [[ test -z "${or_zone}" ]] || [[ test -z "${dt_zone}" ]]
+if [ -z ${vm_nm} ] || [ -z ${or_zone} ] || [ -z ${dt_zone} ]
 then
       echo "One or more required variables are empty, the script cannot continue."
 else
@@ -61,20 +61,25 @@ else
     boot_disk_nm=$(gcloud compute instances describe ${vm_nm} --zone=${or_zone} | grep -C 5 "boot: true" | grep deviceName: | awk -F: '{print $2}')
 
     #
-    # Create the disk snap into the destination zone
+    # Create the disk snap 
     #
     snpsht_nm=${vm_nm}-bootreplicadisk
     gcloud compute disks snapshot ${boot_disk_nm} --zone=${or_zone} --snapshot-names=${vm_nm}-bootdisk
 
     #
-    # Execute the snapshot (set the snapshot name as well)
+    # Create the data disk snap
     #
-    gcloud compute disks create ${snapsht_nm} --zone=${dt_zone} --source-snapshot=${vm_nm}-bootdisk
+    data_snpsht_nm=${vm_nm}-datareplicadisk
+    gcloud compute disks snapshot ${disk_nm} --zone=${or_zone} --snapshot-names=${vm_nm}-datareplicadisk
+
+    #
+    # Execute the disk creation in destination zone
+    #
+    gcloud compute disks create ${snpsht_nm} --zone=${dt_zone} --source-snapshot=${vm_nm}-bootdisk
     
     #
     # Create the VM with a boot disk already created and add the additional disk created from the snapshot
     #
-    data_dsk_nm=${vm_nm}-datadisk
     data_rpl_nm=${vm_nm}-datareplicadisk
-    gcloud beta compute instances create ${vm_nm}-replica --zone=${dt_zone} --machine-type=${mch_type} --disk=name=${snpsht_nm},device-name=${snpsht_nm},mode=rw,boot=yes,auto-delete=no --create-disk source-snapshot=${data_dsk_nm},name=${data_dsk_nm},device-name=${data_rpl_nm},auto-delete=no,mode=rw --reservation-affinity=${res_affn}
+    gcloud beta compute instances create ${vm_nm}-replica --zone=${dt_zone} --machine-type=${mch_type} --disk=name=${snpsht_nm},device-name=${snpsht_nm},mode=rw,boot=yes,auto-delete=no --create-disk source-snapshot=${data_snpsht_nm},name=${data_snpsht_nm},device-name=${data_rpl_nm},auto-delete=no,mode=rw --reservation-affinity=${res_affn}
 fi
